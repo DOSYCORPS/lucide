b197c9389b8245d3a3abebb3614b4e57389ad0c4
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2022-11-17 20:54:56 +0800

    Extend querySelectorAll to pierce Shadow DOM
    This is useful for pages where Custom Elements are present and authors wish to use icons inside of a custom element's shadow tree.
    
    This only supports entire selectors within a document (or shadow) boundary, and does not support matching part of a selector outside of a shadow tree, while matching the rest of it inside that shadow tree. This limited functionality was chosen as the simplest way to extend the  package to support  across shadow boundaries while only having to call it once.

--- packages/lucide/src/lucide.js
+++ packages/lucide/src/lucide.js
@@ -16,7 +16,7 @@ const createIcons = ({ icons = {}, nameAttr = 'icon-name', attrs = {} } = {}) =>
     throw new Error('`createIcons()` only works in a browser environment.');
   }
 
-  const elementsToReplace = document.querySelectorAll(`[${nameAttr}]`);
+  const elementsToReplace = querySelectorAll(`[${nameAttr}]`);
 
   Array.from(elementsToReplace).forEach((element) =>
     replaceElement(element, { nameAttr, icons, attrs }),
@@ -35,3 +35,37 @@ export { default as createElement } from './createElement';
 */
 export { allIcons as icons };
 export * from './icons';
+
+/**
+ * Helper to enable querySelectorAll to recrusively pierce all Shadow DOM children 
+ * @param {string} selectors
+ * @returns {NodeList}
+ *
+ * Note: This only supports the querying of an *entire selector* in the current document
+ * and all shadow DOM children recursively.
+ * It *does not support* matching a selector's prefix in a shadow ancestor, and matching
+ * the suffix of the selector inside the shadow tree.
+ */
+const querySelectorAll = (selectors) => {
+  return explodeContext(document).flatMap(context => query(selectors, context));
+
+  function query(selectors, context) {
+    return Array.from(context.querySelectorAll(selectors));
+  }
+
+  function explodeContext(context, docs = []) {
+    docs.push(context);
+
+    const domIterator = document.createTreeWalker(context, NodeFilter.SHOW_ELEMENT);
+
+    let node;
+
+    while(node = domIterator.nextNode()) {
+      if ( node.shadowRoot ) {
+        explodeContext(node.shadowRoot, docs);
+      }
+    }
+
+    return docs;
+  }
+}
